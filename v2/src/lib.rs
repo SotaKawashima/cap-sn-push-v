1c8df7271849d39669f66d194dbc4bfc7137de43
Masaaki Miyashita <m.mysht118@gmail.com>
2024-08-08 22:11:43 +0900

    WIP: implement v2 executor

--- v2/src/lib.rs
+++ v2/src/lib.rs
@@ -1,22 +1,32 @@
+use std::borrow::Cow;
+
 use base::{
     executor::{
         AgentExtTrait, AgentIdx, AgentWrapper, Executor, InstanceExt, InstanceWrapper, Memory,
     },
+    info::InfoContent,
     opinion::{AccessProb, MyFloat, Trusts},
 };
 use graph_lib::prelude::{Graph, GraphB};
-use rand::Rng;
+use rand::{seq::index::sample, Rng};
 use rand_distr::{Distribution, Exp1, Open01, Standard, StandardNormal};
+use subjective_logic::{marr1, marr_d1, mul::labeled::OpinionD1};
 
 pub async fn start() -> anyhow::Result<()> {
     Ok(())
 }
 
-struct Exec {
-    graph: GraphB,
+struct Communities<V> {
+    misinfo: Vec<(usize, V)>,
 }
 
-impl<V> Executor<V, AgentExt, Instance> for Exec
+struct Exec<V> {
+    graph: GraphB,
+    communities: Communities<V>,
+    produce_prob: V,
+}
+
+impl<V> Executor<V, AgentExt, Instance> for Exec<V>
 where
     V: MyFloat,
     Open01: Distribution<V>,
@@ -43,9 +53,12 @@ impl<V> AgentExtTrait<V> for AgentExt {
     }
 }
 
-struct Instance;
+struct Instance {
+    // info_contents: Vec<InfoContent<V>>,
+    misinfo_producers: Vec<usize>,
+}
 
-impl<V, R> InstanceExt<V, R, Exec> for Instance
+impl<V, R> InstanceExt<V, R, Exec<V>> for Instance
 where
     V: MyFloat,
     Open01: Distribution<V>,
@@ -54,7 +67,7 @@ where
     Exp1: Distribution<V>,
     R: Rng,
 {
-    fn from_exec(exec: &Exec) -> Self {
+    fn from_exec(exec: &Exec<V>) -> Self {
         todo!()
     }
 
@@ -63,20 +76,31 @@ where
     }
 
     fn get_producers_with<'a>(
-        ins: &mut InstanceWrapper<'a, Exec, V, R, Self>,
-        t: u32,
-    ) -> Vec<(AgentIdx, &'a base::info::InfoContent<V>)> {
-        todo!()
+        ins: &mut InstanceWrapper<'a, Exec<V>, V, R, Self>,
+        _: u32,
+    ) -> Vec<(AgentIdx, Cow<'a, InfoContent<V>>)> {
+        let mut v = Vec::new();
+        ins.ext.misinfo_producers.retain(|&agent_idx| {
+            if ins.exec.produce_prob >= ins.rng.gen() {
+                return true;
+            }
+            let c = InfoContent::Misinfo {
+                op: OpinionD1::vacuous_with(marr_d1![V::one(), V::zero()]),
+            };
+            v.push((agent_idx.into(), Cow::Owned(c)));
+            false
+        });
+        v
     }
 
     fn get_informer<'a>(
-        ins: &mut InstanceWrapper<'a, Exec, V, R, Self>,
+        ins: &mut InstanceWrapper<'a, Exec<V>, V, R, Self>,
     ) -> (Trusts<V>, AccessProb<V>) {
         todo!()
     }
 
     fn get_sharer<'a>(
-        ins: &mut InstanceWrapper<'a, Exec, V, R, Self>,
+        ins: &mut InstanceWrapper<'a, Exec<V>, V, R, Self>,
         info_idx: usize,
     ) -> (Trusts<V>, AccessProb<V>) {
         todo!()
